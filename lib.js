e9104eb3a2523af6e6e15f0648e47549a7564c8b
Eduardo Sánchez <eduardosr1599@gmail.com>
2022-10-04 17:47:26 -0400

    Add createCard function

--- lib.js
+++ lib.js
@@ -1,5 +1,6 @@
 let myLibrary=[];
-let popUp=document.querySelector(".pop-up")
+let popUp=document.querySelector(".pop-up");
+let cardsHolder=document.querySelector(".cards");
 
 function Book(title,author,pages){
     this.title=title;
@@ -19,6 +20,25 @@ function addBookToLibrary(){
     pagesInput.value="";
 }
 
+let harryPotter=new Book("Harry Potter and the Philosopher's Stone","J.K Rowling","233");
+myLibrary.push(harryPotter);
+let readyPlayerOne=new Book("Ready Player One","Ernest Cline","374");
+myLibrary.push(readyPlayerOne);
+let sapiens=new Book("Sapiens: A Brief History of Humankind", "Yuval Noah Harari","512");
+myLibrary.push(sapiens)
+
+function createCard(book){
+    let card=document.createElement("div");
+    for (let property in book){
+        let newItem=document.createElement("p");
+        newItem.textContent=`${property} : ${book[property]}`;
+        card.appendChild(newItem);
+    }
+    cardsHolder.appendChild(card);
+}
+ for (let i=0;i<myLibrary.length;i++){
+    createCard(myLibrary[i]);
+ }
 // function createForm(){
 //     let newForm=document.createElement("form");
 //     let titleInput=document.createElement("input");
